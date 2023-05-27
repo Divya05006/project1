4bddf9444695f220e4bb5869f5a34bf07fd5d2cb
Divya05006 <divyakmyogipoo@gmail.com>
2023-05-27 10:41:26 +0530

    hi

---
+++ src/main/java/iocbasics/Bike.java
@@ -0,0 +1,13 @@
+package iocbasics;
+
+public class Bike 
+{
+	void ride()
+	{
+		System.out.println("long drive");
+	}
+	void drive()
+	{
+		System.out.println("going with...");
+	}
+}
---
+++ src/main/java/iocbasics/Mobile.java
@@ -0,0 +1,13 @@
+package iocbasics;
+
+public class Mobile 
+{
+	void call()
+	{
+		System.out.println("ringing");
+	}
+	void phone()
+	{
+		System.out.println("yes it is");
+	}
+}
